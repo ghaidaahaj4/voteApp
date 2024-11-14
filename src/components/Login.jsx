c314c5a2b9998484975eab20a0b2a0e0849b839b
ghaidaahaj42 <gaidaahaj2@gmail.com>
2024-11-14 14:27:13 +0200

    working on login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+export default function Login() {
+  return (
+    <form>
+      <div className="foromTitle">
+        <span className="material-symbols-outlined mainImg">how_to_vote</span>
+        <h1 className="">Please sign in</h1>
+      </div>
+
+      <div className="inputForom">
+        <label>Full Name</label>
+        <input type="name" className="" placeholder="MyName" />
+      </div>
+      <div className="inputForom">
+        <label>Password</label>
+        <input type="password" className="" placeholder="Password" />
+      </div>
+
+      <button className="" type="submit">
+        Sign in
+      </button>
+      <p>© Elections Day 2024</p>
+    </form>
+  );
+}
