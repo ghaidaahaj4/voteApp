375162ad5edf0b0c58ea90c7b705a101003a4937
ghaidaahaj42 <gaidaahaj2@gmail.com>
2024-11-14 22:30:42 +0200

    make the done button

--- src/components/Voting/CandidateCard.jsx
+++ src/components/Voting/CandidateCard.jsx
@@ -1,24 +1,32 @@
-// CandidateCard.js
-import React, { useState } from "react";
+/* eslint-disable react/prop-types */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFish } from "@fortawesome/free-solid-svg-icons";
-
-export default function CandidateCard() {
-  const [buttonStatus, setStatus] = useState(true);
+import { useState } from "react";
+export default function CandidateCard({ voted, handle }) {
+  const [buttonStatus, setStatus] = useState(0);
 
   function handleClick() {
-    setStatus((prev) => !prev);
+    setStatus((prev) => {
+      console.log(voted);
+      if (prev === 0) return 1;
+      if (prev === 1) {
+        handle(true);
+        return 2;
+      }
+    });
   }
 
   return (
     <div>
       <FontAwesomeIcon icon={faFish} />
       <h1>NAME</h1>
-      {buttonStatus ? (
+      {buttonStatus === 0 && !voted && (
         <button onClick={handleClick}>Vote</button>
-      ) : (
-        <button onClick={handleClick}>You voted</button>
       )}
+      {buttonStatus === 1 && !voted && (
+        <button onClick={handleClick}>Are You Sure</button>
+      )}
+      {voted && <button>Done</button>}
     </div>
   );
 }
--- src/components/Voting/VotingPage.jsx
+++ src/components/Voting/VotingPage.jsx
@@ -1,16 +1,20 @@
-// VotingPage.js
-import React from "react";
+/* eslint-disable no-undef */
 import CandidateCard from "./CandidateCard";
+import { useState } from "react";
 
 export default function VotingPage() {
+  const [voted, setIsVoted] = useState(false);
+  function handleVoteFinalClick() {
+    setIsVoted(true);
+  }
   return (
     <>
       <h2>Hello User</h2>
       <div className="VotingPage">
-        <CandidateCard />
-        <CandidateCard />
-        <CandidateCard />
-        <CandidateCard />
+        <CandidateCard voted={voted} handle={handleVoteFinalClick} />
+        <CandidateCard voted={voted} handle={handleVoteFinalClick} />
+        <CandidateCard voted={voted} handle={handleVoteFinalClick} />
+        <CandidateCard voted={voted} handle={handleVoteFinalClick} />
       </div>
     </>
   );
