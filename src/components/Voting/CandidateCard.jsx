4b3f8ae7313362c2229153de384c7ba0d42b3dac
ghaidaahaj42 <gaidaahaj2@gmail.com>
2024-11-14 22:43:46 +0200

    add the inbetween button

--- src/components/Voting/CandidateCard.jsx
+++ src/components/Voting/CandidateCard.jsx
@@ -2,31 +2,36 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFish } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-export default function CandidateCard({ voted, handle }) {
+export default function CandidateCard({ voted, handle, middle, handleMiddle }) {
   const [buttonStatus, setStatus] = useState(0);
 
   function handleClick() {
     setStatus((prev) => {
-      console.log(voted);
-      if (prev === 0) return 1;
+      if (prev === 0) {
+        return 1;
+      }
       if (prev === 1) {
-        handle(true);
         return 2;
       }
     });
+
+    if (buttonStatus === 0) {
+      handleMiddle(true);
+    }
+    if (buttonStatus === 1) {
+      handle(true);
+    }
   }
 
   return (
     <div>
       <FontAwesomeIcon icon={faFish} />
       <h1>NAME</h1>
-      {buttonStatus === 0 && !voted && (
-        <button onClick={handleClick}>Vote</button>
-      )}
-      {buttonStatus === 1 && !voted && (
-        <button onClick={handleClick}>Are You Sure</button>
-      )}
+
+      {!middle && <button onClick={handleClick}>Vote</button>}
+      {middle && !voted && <button onClick={handleClick}>Are You Sure</button>}
       {voted && <button>Done</button>}
+      {buttonStatus === 1 && <p>HERE</p>}
     </div>
   );
 }
--- src/components/Voting/VotingPage.jsx
+++ src/components/Voting/VotingPage.jsx
@@ -3,18 +3,42 @@ import CandidateCard from "./CandidateCard";
 import { useState } from "react";
 
 export default function VotingPage() {
+  const [middleVote, setMiddleVote] = useState(false);
   const [voted, setIsVoted] = useState(false);
   function handleVoteFinalClick() {
     setIsVoted(true);
+  }
+  function handleMiddle() {
+    setMiddleVote(true);
   }
   return (
     <>
       <h2>Hello User</h2>
       <div className="VotingPage">
-        <CandidateCard voted={voted} handle={handleVoteFinalClick} />
-        <CandidateCard voted={voted} handle={handleVoteFinalClick} />
-        <CandidateCard voted={voted} handle={handleVoteFinalClick} />
-        <CandidateCard voted={voted} handle={handleVoteFinalClick} />
+        <CandidateCard
+          voted={voted}
+          handle={handleVoteFinalClick}
+          middle={middleVote}
+          handleMiddle={handleMiddle}
+        />
+        <CandidateCard
+          voted={voted}
+          handle={handleVoteFinalClick}
+          middle={middleVote}
+          handleMiddle={handleMiddle}
+        />
+        <CandidateCard
+          voted={voted}
+          handle={handleVoteFinalClick}
+          middle={middleVote}
+          handleMiddle={handleMiddle}
+        />
+        <CandidateCard
+          voted={voted}
+          handle={handleVoteFinalClick}
+          middle={middleVote}
+          handleMiddle={handleMiddle}
+        />
       </div>
     </>
   );
