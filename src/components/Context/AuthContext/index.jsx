8b1155536d0f4986cae23396efeb25b28489afe8
ghaidaahaj42 <gaidaahaj2@gmail.com>
2024-11-15 10:53:43 +0200

    firebase.js

--- src/components/Context/AuthContext/index.jsx
+++ src/components/Context/AuthContext/index.jsx
@@ -1,6 +1,37 @@
-import React, { useState } from "react";
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect, useContext } from "react";
+import { auth } from "../../../firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 const AuthContext = React.createContext();
+export function useAuth() {
+  return useContext(AuthContext);
+}
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
-  cosnt[(userLoggidIn, setUserLogIn)] = useState(null);
+  const [userLoggidIn, setUserLogIn] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsbscribe = onAuthStateChanged(auth, initilizeUser);
+    return unsbscribe;
+  }, []);
+  async function initilizeUser(user) {
+    if (user) {
+      setCurrentUser({ ...user });
+      setUserLogIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLogIn(false);
+    }
+    setLoading(false);
+  }
+  const value = {
+    currentUser,
+    userLoggidIn,
+    loading,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
